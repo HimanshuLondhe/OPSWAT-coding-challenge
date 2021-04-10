0a5f1d21d62a4f1a21cbc65ebea8cd5a60c0b794
Himanshu Londhe <himanshu.londhe@ardentsec.com>
2021-04-09 21:42:22 -0400

    Final Update

--- properties.py
+++ properties.py
@@ -1,4 +1,4 @@
-apikey = "584eb607b17a4634e1aa535188c69daa"
+apikey = ""
 commonurl = "https://api.metadefender.com/v4/file/"
 callbackurl = "https://webhook.site/b003869b-f444-4787-88fa-5a99087c45d8"
 callbackmetadefender = "https://api.metadefender.com/v4/file/webhooks/"
